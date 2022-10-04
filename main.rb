17ca1ff4258623367ddd4e3f4b3837e27148bfb6
ledsun <shigeru.nakajima@gmail.com>
2022-10-04 23:44:41 +0900

    頻出文字に色をつけます。

--- main.rb
+++ main.rb
@@ -19,15 +19,27 @@ search_button.addEventListener "click" do
   correct_places = document.getElementById("correct_places")[:value]
   matched = WordleSeach.search exclude, included, correct_places
 
+  frequency_chars = matched.join
+    .chars
+    .tally
+    .sort_by { _2 }
+    .reverse
+    .take(5)
+    .map { _1[0] }
+    .join
+
   html = matched.map do |word|
     heiglighted_word = word.chars.map do |char|
       if included.include? char
         "<span class='text-red-500'>#{char}</span>"
+      elsif frequency_chars.include? char
+        "<span class='text-green-500'>#{char}</span>"
       else
         char
       end
     end.join
     template.result_with_hash word:, heiglighted_word:
   end.join
+
   document.getElementById("result")[:innerHTML] = html
 end
